266fab3606fab732d8fa82eccb6757a19f984dcd
aniketqw <aniket21stcentury@gmail.com>
2024-03-22 19:46:40 +0530

    first

---
+++ CODEFORCES/PRACTICE/282A/Main.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+            int n = sc.nextInt(); // Input variable
+            int k=0;
+            while(n-->0)
+            {
+                String s=sc.nextLine();
+                if(s.charAt(1)=='+')
+                k++;
+                else
+                k--;
+            }
+            System.out.println(k);
+
+
+    }
+   
+}
